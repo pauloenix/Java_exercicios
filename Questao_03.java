0789d92129d967cbd5a657e4216f1217a299c0d7
pauloenix <41697212+pauloenix@users.noreply.github.com>
2021-10-11 23:16:31 -0300

    Update Questao_03.java

--- Questao_03.java
+++ Questao_03.java
@@ -3,28 +3,16 @@ package av1;
 public class Questao_03 {
 
 	public static void main(String[] args) {
-		Copo c1 = new Copo();
-		Copo c2 = new Copo();
-		
-		
-		c1.setCor("roxo");
-		c2.setCor("amarelo");
-		
-		c1.setCapacidadeAtual(350);
-		c2.setCapacidadeAtual(0);
+		Copo c1 = new Copo(350,350,"roxo");
+		Copo c2 = new Copo(350,0, "azul");
 		
 		c1.retirarLiquido(350);
 		c2.adicionarLiquido(350);
 		
-		
-		
 		c1.obterInformacoes();
-		
-		
 		c2.obterInformacoes();
 		
-				
-
+			
 	}
 
 }
